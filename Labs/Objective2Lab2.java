ab052f4d91cacd3ed6f765d6af6fde69e73e8c2f
Brendan Duffy <brendan.p.duffy5@gmail.com>
2020-12-31 16:16:10 -0700

    O2,L2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brendan Duffy";
+    int age = 41;
+    String state = "New Jersey";
+    System.out.println("Name:");
+    System.out.println(name);
+    System.out.println("Age:");
+    System.out.println(age);
+    System.out.println("State:");
+    System.out.println(state);
+  }
+}
