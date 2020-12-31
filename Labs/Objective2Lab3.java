b4d95e565accead91e08e1ca681c4824a8c8d3b4
Brendan Duffy <brendan.p.duffy5@gmail.com>
2020-12-31 15:46:31 -0700

    O2,L3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "Charlie";
+    System.out.println(myVariable);
+    myVariable = "Mia";
+    System.out.println(myVariable);
+  }
+}
