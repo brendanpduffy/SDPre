626ea53333b5aeb6b12c0cc1d1603a95249e9bd9
Brendan Duffy <brendan.p.duffy5@gmail.com>
2020-12-31 16:46:18 -0700

    O3,L1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1{
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 3;
+    num2 = 4;
+    num3 = 5;
+    average = (3+4+5)/3;
+    System.out.println(average);
+
+  }
+}
