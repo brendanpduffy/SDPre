f85d361aaa96fd350545a98db1825e3675dd0b46
Brendan Duffy <brendan.p.duffy5@gmail.com>
2021-01-01 13:03:50 -0700

    O5,L4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Please enter a number: ");
+    int userNum = scanner.nextInt();
+    int x = userNum % 2;
+    if(x == 0) {
+      System.out.println("The number " + userNum + " is even");
+    }
+    else if (x != 0){
+      System.out.println("The number " + userNum + " is odd");
+    }
+  scanner.close();
+  }
+}
